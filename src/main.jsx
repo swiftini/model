13b5533fec3449c5c8fde42565e4573ce364552a
Leif Holtzman <swiftini@gmail.com>
2025-12-16 11:12:29 -0600

    Add Vite React project structure

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FinancialModel from './FinancialModel'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FinancialModel />
+  </React.StrictMode>,
+)
